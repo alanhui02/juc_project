6a4cccea995193519ef9db4f47868ab6229164bb
alan <978369868@qq.com>
2021-10-04 11:49:38 +0800

    Interrupt init

---
+++ src/main/java/com/y/juc/look/com/y/juc/interrupt/InterruptLookDemo.java
@@ -0,0 +1,14 @@
+package com.y.juc.look.com.y.juc.interrupt;
+
+/**
+ * @Author yao
+ * @Date 2021/10/4
+ * 线程中断
+ *
+ */
+public class InterruptLookDemo {
+
+	public static void main(String[] args) {
+		Thread.interrupted();
+	}
+}
